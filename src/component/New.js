eed69efca985757e691b613c11c91d70eac3275c
jongju-Lee <jeje7@naver.com>
2023-09-29 21:56:19 +0900

    /New 화면 구성중

--- src/component/New.js
+++ src/component/New.js
@@ -1,10 +1,38 @@
 import React from "react";
+import MyHeader from "../ui/MyHeader";
+import MyButton from "../ui/MyButton";
+import { useNavigate } from "react-router-dom";
 
 const New = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>New</h1>
-      <p>이곳은 일기 작성 페이지 입니다.</p>
+    <div className="New">
+      <div className="header">
+        <MyHeader
+          leftChild={
+            <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+          }
+          headText={"새로운 일기 쓰기"}
+        />
+      </div>
+      <div className="new_date">
+        <h3>오늘은 언제인가요?</h3>
+        <select>
+          <option></option>
+        </select>
+      </div>
+      <div className="new_emotion">
+        <h3>오늘의 감정</h3>
+      </div>
+      <div className="new_textarea">
+        <h3>오늘의 일기</h3>
+        <textarea />
+      </div>
+      <div className="btn_wrapper">
+        <MyButton />
+        <MyButton />
+      </div>
     </div>
   );
 };
