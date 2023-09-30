135fcdf2b09e8b7deff13c8c8979d0de07796287
jongju-Lee <jeje7@naver.com>
2023-09-30 14:32:35 +0900

    /New 완료! /Edit 시작

--- src/component/New.js
+++ src/component/New.js
@@ -1,38 +1,9 @@
-import React from "react";
-import MyHeader from "../ui/MyHeader";
-import MyButton from "../ui/MyButton";
-import { useNavigate } from "react-router-dom";
+import DiaryEditor from "./DiaryEditor";
 
 const New = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="New">
-      <div className="header">
-        <MyHeader
-          leftChild={
-            <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
-          }
-          headText={"새로운 일기 쓰기"}
-        />
-      </div>
-      <div className="new_date">
-        <h3>오늘은 언제인가요?</h3>
-        <select>
-          <option></option>
-        </select>
-      </div>
-      <div className="new_emotion">
-        <h3>오늘의 감정</h3>
-      </div>
-      <div className="new_textarea">
-        <h3>오늘의 일기</h3>
-        <textarea />
-      </div>
-      <div className="btn_wrapper">
-        <MyButton />
-        <MyButton />
-      </div>
+      <DiaryEditor />
     </div>
   );
 };
