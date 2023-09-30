02ced21b013e319ac6672691d1d61c7bfbfc48a5
jongju-Lee <jeje7@naver.com>
2023-10-01 00:43:43 +0900

    /diary 작성중 12 : 40

--- src/component/Diary.js
+++ src/component/Diary.js
@@ -1,16 +1,78 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import MyHeader from "../ui/MyHeader";
+import MyButton from "../ui/MyButton";
+import { DiaryStateContext } from "../App";
+import { getStringDate } from "../util/date";
 
 const Diary = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
-  console.log(id);
+  const diaryList = useContext(DiaryStateContext);
+  const [data, setData] = useState();
 
-  return (
-    <div>
-      <h1>Diary</h1>
-      <p>이곳은 일기 상세 페이지 입니다.</p>
-    </div>
-  );
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+      if (targetDiary) {
+        setData(targetDiary);
+      } else {
+        alert("존재하지 않는 일기 입니다.");
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
+
+  if (!data) {
+    return <div className="DiaryPage">로딩중입니다...</div>;
+  } else {
+    return (
+      <div className="DiaryPage">
+        <MyHeader
+          headText={`${getStringDate(new Date(data.date))} 일기`}
+          leftChild={
+            <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+          }
+          rightChild={
+            <MyButton
+              text={"수정하기"}
+              onClick={() => navigate(`/edit/${data.id}`)}
+            />
+          }
+          // 2023. 10. 01 오전 12: 40 여기부터 하면 됨~
+          // 페이지 구현 - 일기 상세(/diary)
+          // 9분 13초부터 보면됨
+        />
+      </div>
+    );
+  }
+
+  // return (
+  //   <div>
+  //     <MyHeader
+  //       headText={`일기`}
+  //       leftChild={
+  //         <MyButton
+  //           text={"< 뒤로가기"}
+  //           onClick={() => navigate(-1, { replace: true })}
+  //         />
+  //       }
+  //       rightChild={
+  //         <MyButton text={"수정하기"} onClick={() => navigate(`/edit/${id}`)} />
+  //       }
+  //     />
+  //     <section>
+  //       <h4>오늘의 감정</h4>
+  //       <img src={process.env.PUBLIC_URL + `/assets/emotion${id}.png`} />
+  //     </section>
+  //     <section>
+  //       <h4>오늘의 일기</h4>
+  //       <textarea />
+  //     </section>
+  //   </div>
+  // );
 };
 
 export default Diary;
